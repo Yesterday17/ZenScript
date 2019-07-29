a9c200109850360958411c665a003440cf26afae
Yesterday17 <t@yesterday17.cn>
2019-07-29 11:40:40 +0800

    Catch error in forEach of items.
    Fix #10.

--- server/utils/zsrcFile.ts
+++ server/utils/zsrcFile.ts
@@ -22,46 +22,62 @@ export function reloadRCFile(connection: Connection) {
 
     // Reload Mods
     zGlobal.mods.clear();
-    zGlobal.rcFile.mods.forEach(value => {
-      zGlobal.mods.set(value.modid, value);
-    });
+    if (zGlobal.rcFile.mods) {
+      zGlobal.rcFile.mods.forEach(value => {
+        zGlobal.mods.set(value.modid, value);
+      });
+    }
 
     // Items
     zGlobal.items.clear();
-    zGlobal.rcFile.items.forEach(value => {
-      const key = [
-        value.resourceLocation.domain,
-        value.resourceLocation.path,
-        value.metadata,
-      ].join(':');
-      zGlobal.items.set(key, value);
-    });
+    if (zGlobal.rcFile.items) {
+      zGlobal.rcFile.items.forEach(value => {
+        try {
+          const key = [
+            value.resourceLocation.domain,
+            value.resourceLocation.path,
+            value.metadata,
+          ].join(':');
+          zGlobal.items.set(key, value);
+        } catch (e) {
+          connection.console.error(
+            `Failed to load item: ${value.unlocalizedName}`
+          );
+        }
+      });
+    }
 
     // Enchantments
     zGlobal.enchantments.clear();
-    zGlobal.rcFile.enchantments.forEach(value => {
-      if (!zGlobal.enchantments.has(value.resourceLocation.domain)) {
-        zGlobal.enchantments.set(value.resourceLocation.domain, [value]);
-      } else {
-        zGlobal.enchantments.get(value.resourceLocation.domain).push(value);
-      }
-    });
+    if (zGlobal.rcFile.enchantments) {
+      zGlobal.rcFile.enchantments.forEach(value => {
+        if (!zGlobal.enchantments.has(value.resourceLocation.domain)) {
+          zGlobal.enchantments.set(value.resourceLocation.domain, [value]);
+        } else {
+          zGlobal.enchantments.get(value.resourceLocation.domain).push(value);
+        }
+      });
+    }
 
     // Entities
     zGlobal.entities.clear();
-    zGlobal.rcFile.entities.forEach(value => {
-      if (!zGlobal.entities.has(value.resourceLocation.domain)) {
-        zGlobal.entities.set(value.resourceLocation.domain, [value]);
-      } else {
-        zGlobal.entities.get(value.resourceLocation.domain).push(value);
-      }
-    });
+    if (zGlobal.rcFile.entities) {
+      zGlobal.rcFile.entities.forEach(value => {
+        if (!zGlobal.entities.has(value.resourceLocation.domain)) {
+          zGlobal.entities.set(value.resourceLocation.domain, [value]);
+        } else {
+          zGlobal.entities.get(value.resourceLocation.domain).push(value);
+        }
+      });
+    }
 
     // Fluids
     zGlobal.fluids.clear();
-    zGlobal.rcFile.fluids.forEach(value =>
-      zGlobal.fluids.set(value.unlocalizedName, value)
-    );
+    if (zGlobal.rcFile.fluids) {
+      zGlobal.rcFile.fluids.forEach(value =>
+        zGlobal.fluids.set(value.unlocalizedName, value)
+      );
+    }
   } catch (e) {
     connection.console.error(e.message);
   }
